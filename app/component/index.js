07dcac7afad9f3148f11d79f3a1db527c03b4f72
amulyk <a.mulyk@lectrum.io>
2017-12-27 15:49:14 +0200

    closed #27

--- app/component/index.js
+++ app/component/index.js
@@ -1,5 +1,6 @@
-export default ( text = "Hello world!" ) => {
-    const element = document.createElement("div");
+export default (text = 'Hello world!') => {
+    const element = document.createElement('div');
+
     element.innerHTML = text;
 
     return element;
