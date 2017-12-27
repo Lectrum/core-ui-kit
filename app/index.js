0eeccd07056b8924376431d6bd0ee2359c1d5225
amulyk <a.mulyk@lectrum.io>
2017-12-27 16:04:15 +0200

    closed #28

--- app/index.js
+++ app/index.js
@@ -1,13 +1,12 @@
 // Core
-import component from './component';
-function* test () {
-    yield console.log(1);
-    yield console.log(2);
-    yield console.log(3);
+import component from "./component";
+function* test() {
+  yield console.log(1);
+  yield console.log(2);
+  yield console.log(3);
 }
 
 const t = test();
-
 t.next();
 t.next();
 t.next();
